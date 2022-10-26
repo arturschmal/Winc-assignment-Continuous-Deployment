cdbc7e654a89c3db2b7cec12a56b76860c459cca
Artur Schmal <artur@originaltype.com>
2022-10-26 21:57:55 +0200

    update main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 from flask import Flask
 from flask import Flask, redirect, render_template, request, session, url_for
 
-# Create a Flask app inside `app`
 app = Flask(__name__)
 
-# Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
     return render_template("index.html", title="Index")
